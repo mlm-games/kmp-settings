4a73500197c3187e4638649ab441f8b668ce1bfc
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-12-27 17:56:53 +0530

    Store android datastore prefs in datastore/ folder (BREAKING, do not migrate others rn...)

--- settings-core/src/androidMain/kotlin/io/github/mlmgames/settings/core/datastore/CreateDataStore.android.kt
+++ settings-core/src/androidMain/kotlin/io/github/mlmgames/settings/core/datastore/CreateDataStore.android.kt
@@ -3,10 +3,11 @@ package io.github.mlmgames.settings.core.datastore
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 
 fun createSettingsDataStore(context: Context, name: String): DataStore<Preferences> =
-  createDataStore(
-    producePath = {
-      context.filesDir.resolve("$name.preferences_pb").absolutePath
-    }
-  )
+    createDataStore(
+        producePath = {
+            context.preferencesDataStoreFile(name).absolutePath
+        }
+    )
