010341bf1a763a9389eb5378a8ce8fa895aef993
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-03-16 11:32:59 +0530

    fix wasm datastore not being gotten (only was being set)

--- settings-core/src/wasmJsMain/kotlin/io/github/mlmgames/settings/core/datastore/CreateDataStore.wasmJs.kt
+++ settings-core/src/wasmJsMain/kotlin/io/github/mlmgames/settings/core/datastore/CreateDataStore.wasmJs.kt
@@ -3,9 +3,11 @@ package io.github.mlmgames.settings.core.datastore
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 
+private val wasmStores = mutableMapOf<String, DataStore<Preferences>>()
+
 fun createSettingsDataStore(name: String): DataStore<Preferences> =
-    createDataStore(
-        producePath = {
-            "/$name.preferences_pb"
-        }
-    )
+    wasmStores.getOrPut(name) {
+        createDataStore(
+            producePath = { "/$name.preferences_pb" }
+        )
+    }
